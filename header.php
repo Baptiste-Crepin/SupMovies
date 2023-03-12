923a0292a17c6c8e1e5ff9199e3826aa980b96d8
Grid13 <martin.pierrache59161@gmail.com>
2023-03-12 21:52:08 +0100

    button

--- header.php
+++ header.php
@@ -18,8 +18,8 @@
     </a>
     <form class="search-bar" action="search_bar.php" method="GET">
       <input type="text" id="search-input" name="search" placeholder="Rechercher...">
-      <button type="submit" name="submit">Search</button>
-      <button type="submit" name="submit_director">Search by director</button>
+      <button class="button primary-button" type="submit" name="submit">Search</button>
+      <button class="button secondary-button" type="submit" name="submit_director">Search by director</button>
     </form>
     <a class="genre" href="genre.php">Genres</a>
     <!-- <span></span> -->
